6362273f272e69c4178fe607b487902a4b9b6e69
Diganto Bhowmik <52887469+DigantoBhowmik@users.noreply.github.com>
2021-07-07 22:15:11 +0600

    done

--- bfs&dfs.py
+++ bfs&dfs.py
@@ -1,13 +1,49 @@
 graph = {
-    "S": {"B": 1, "A": 2,"G": 9},
-    "B": {"E": 4, "D": 2},
-    "A": {"D": 3, "C": 2},
-    "D": {"G": 4},
-    "C": {"G": 4},
+    "Arad": {"Timisoara": 118, "Sibiu": 140,"Zerind": 75},
+    "Zerind": {"Arad": 75, "Oradea": 71},
+    "Oradea": {"Zerind": 71, "Sibiu": 151},
+    "Timisoara": {"Arad": 118, "Lugoj": 111},
+    "Lugoj": {"Timisoara": 111, "Mehadia":70},
+    "Mehadia": {"Lugoj": 70, "Dobreta": 75},
+    "Dobreta": {"Mehadia":75, "Craiova":120},
+    "Craiova": {"Dobreta": 120, "RimnicuVilcea": 146, "Pitesi": 138},
+    "RimnicuVilcea": {"Craiova": 146, "Pitesi": 97, "Sibiu":80},
+    "Sibiu": {"Arad": 140, "Oradea":151, "RimnicuVilcea": 80, "Fagaras": 99},
+    "Fagaras": {"Sibiu": 99, "Bucharest":211},
+    "Pitesi": {"Bucharest": 101, "RimnicuVilcea": 97, "Craiova": 138},
+    "Bucharest": {"Pitesi": 101, "Fagaras": 211, "Giurgiu": 90, "Urziceni": 85},
+    "Giurgiu": {"Bucharest": 90},
+    "Urziceni": {"Bucharest": 85, "Hirsova": 98, "Vaslui": 142},
+    "Hirsova": {"Urziceni": 98, "Eforie": 86},
+    "Eforie": {"Hirsova": 86},
+    "Vaslui": {"Urziceni": 142, "Iasi": 92},
+    "Iasi": {"Vaslui": 92, "Neamt": 87},
+    "Neamt": {"Iasi": 87}
 }
 
 
-
+heuristicSLD={
+    "Arad": 366,
+    "Bucharest": 0,
+    "Craiova": 160,
+    "Dobreta": 242,
+    "Eforie": 161,
+    "Fagaras": 176,
+    "Giurgiu": 77,
+    "Hirsova": 151,
+    "Iasi": 226,
+    "Lugoj": 244,
+    "Mehadia": 241,
+    "Neamt": 234,
+    "Oradea": 380,
+    "Pitesi": 100,
+    "RimnicuVilcea": 193,
+    "Sibiu": 253,
+    "Timisoara": 329,
+    "Urziceni": 80,
+    "Vaslui": 199,
+    "Zerind": 374  
+    }
 class graphProblem:
 
     def __init__(self,initial,goal,graph):
@@ -116,7 +152,7 @@ def breadthFirstSearch(gp):
 
                 
 
-gp=graphProblem('S','G',graph)
+gp=graphProblem('Arad','Bucharest',graph)
 
 print ( " Result of BFS " )
 print('===================================')
